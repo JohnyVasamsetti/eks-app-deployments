7966c1b610b450d1c3ff6fdb5b687b676677450d
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-11-02 15:58:17 +0530

    [Johny] Add control plane role name

--- iam.tf
+++ iam.tf
@@ -1,5 +1,6 @@
 # control plane role
 resource "aws_iam_role" "eks_control_plane_role" {
+  name = "ControlPlaneRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
